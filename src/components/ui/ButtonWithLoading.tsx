8e79755a3db7ecea21c6f2307e9297c336f12c74
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-08-04 01:14:50 +0300

    fix(sidebar): correct VariantProps import as type-only to avoid runtime error

---
+++ src/components/ui/ButtonWithLoading.tsx
@@ -0,0 +1,12 @@
+import { Loader2Icon } from "lucide-react";
+import { Button } from "./button";
+
+
+export default function ButtonWithLoading() {
+  return (
+    <Button size="sm" disabled>
+      <Loader2Icon className="animate-spin" />
+      Please wait
+    </Button>
+  )
+}
