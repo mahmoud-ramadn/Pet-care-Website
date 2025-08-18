6aff548147829bbce99610a753d3740bf30794de
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-08-18 21:59:41 +0300

    feat(checkoutpage): finsh ui  with integreation api in checkOut page

--- src/components/DashbordTables/userOrderListing/index.tsx
+++ src/components/DashbordTables/userOrderListing/index.tsx
@@ -5,12 +5,21 @@ import { useMemo } from "react"
 import DataTable from "@/components/ui/data-table"
 
 import { useOrderUser } from "@/hooks/user"
+import { formatPriceEGP } from "@/lib/FormatPriceEGp"
 
 export default function UserOrderListing() {
   const { value: order, loading } = useOrderUser()
   const columns: ColumnDef<Order>[] = useMemo(() => {
     return [
-      { accessorKey: "totalOrderPrice", header: "  total Order Price" },
+      {
+        accessorKey: "totalOrderPrice", header: "  total Order Price",
+         cell: ({ row }) => {
+          const TotalPirc = row?.original.totalOrderPrice
+          if (!TotalPirc) return <span className="text-gray-400"> zero</span>
+
+          return <span>{formatPriceEGP(TotalPirc)}</span>
+        },
+      },
       { accessorKey: "paymentMethodType", header: " payment Method Type" },
       { accessorKey: "isPaidAndDelivered", header: "is Paid And Delivered" },
       {
