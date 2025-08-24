fe6f0f5ab582e690ad8caff8b1bc3a574bb99efd
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-08-24 14:32:05 +0300

    feat(api): remove API key

--- src/pages/Dashborads/admin/index.tsx
+++ src/pages/Dashborads/admin/index.tsx
@@ -3,7 +3,7 @@ import { ChartLine } from "@/components/ui/ChartLine"
 export default function adminDashboard() {
   return (
     <div>
-<h1 className=" font-bold text-4xl  text-blue-400 py-4">Coming soon</h1>
+      <h1 className=" font-bold text-4xl  text-blue-400 py-4">Coming soon</h1>
       <ChartLine />
     </div>
   )
